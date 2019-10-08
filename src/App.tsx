b08f8d7f79fbb49795f45d0773b163ad26469e27
Patrick Weiss <patrick.weiss@officeone.team>
2019-10-08 09:18:36 +0200

    typescript simple asset management

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SimpleAsset from './components/SimpleAsset'
+import mongoose from 'mongoose';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps{}
+
+export interface IAssetData{
+    _id:string;
+    asset_name:string;
+    asset_value:number;
 }
 
-export default App;
+interface IState {
+    assets: JSX.Element[];
+  }
+
+export default class App extends React.PureComponent<IProps, IState> {
+
+  constructor(props:any) {
+    console.log("new App component will be initialized");
+    super(props);
+
+    this.handleCreateAsset = this.handleCreateAsset.bind(this);
+    this.handleDeleteAsset = this.handleDeleteAsset.bind(this);
+
+    const exampleAsset = {
+        _id: mongoose.Types.ObjectId().toString(),
+        asset_name: "This is an example, press Edit to change name and Value",
+        asset_value: 0
+      }
+
+    this.state = {
+        assets: [exampleAsset].map(asset => <SimpleAsset key={asset._id} onDelete={this.handleDeleteAsset} asset={asset} edit={false} />)
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>simple asset management application</h1>
+        <p>to create a new asset click this button:&nbsp;
+          <button onClick={this.handleCreateAsset}>create asset</button>
+        </p>
+        <table>
+          <tbody>
+            <tr><th>description</th><th>value</th><th>action</th></tr>
+            {this.state.assets}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  handleCreateAsset() {
+    console.log("handleCreateAsset invoked");
+    const newAsset:IAssetData = {
+      _id: mongoose.Types.ObjectId().toString(),
+      asset_name: "",
+      asset_value: 0
+    }
+    let newAssets = this.state.assets.slice();
+
+    newAssets.push(<SimpleAsset key={newAsset._id} onDelete={this.handleDeleteAsset} edit={true} asset={newAsset} />);
+
+    this.setState(
+      {
+        assets: newAssets
+      }
+    );
+    console.log(newAsset);
+  }
+
+
+  handleDeleteAsset(event:any) {
+    const IdOfAssetToDelete = event.target.id;
+    console.log("Delete asset with _id:" + IdOfAssetToDelete);
+
+    let newAssets = this.state.assets.filter(asset => {
+      console.log("asset.key:" + asset.key + " IdOfAssetToDelete:" + IdOfAssetToDelete + " " + (asset.key !== IdOfAssetToDelete));
+      return asset.key !== IdOfAssetToDelete;
+    })
+    this.setState(
+      {
+        assets: newAssets
+      }
+    );
+  }
+
+}
---
+++ src/components/SimpleAsset.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import {IAssetData} from '../App';
+
+//this file defines the React component that renders a single asset to the browser window
+//it also contains the logic to change asset properties and save the changes to the database
+//most of the used React framework features are already explained in the comments of App.js
+//so this code hopefully mostly explains itself ...
+
+interface IProps{
+    onDelete:Function;
+    edit:boolean;
+    asset:IAssetData;
+}
+
+
+interface IState{
+    delete_function:any;
+    edit_mode:boolean;
+    asset:IAssetData;
+}
+
+export default class SimpleAsset extends React.PureComponent<IProps, IState> {
+
+    constructor(props:IProps) {
+        super(props);
+
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleValueChange = this.handleValueChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+
+        //one new thing is, that the state will be initialized by properties that are set when the component is created in the container element:
+        //look for "<SimpleAsset key={newAsset._id} onDelete={this.handleDeleteAsset} edit={true} asset={newAsset} />" in App.js
+        this.state = {
+            delete_function: props.onDelete,
+            edit_mode: props.edit,
+            asset: props.asset,
+        }
+    }
+
+    render() {
+        
+        //if the component is in edit mode, it will render different than if it just shows the data
+        
+        if (this.state.edit_mode)
+            return (
+                <tr>
+                    <td><input type="text" name="name" value={this.state.asset.asset_name} onChange={this.handleNameChange} /></td>
+                    <td><input type="number" name="value" value={this.state.asset.asset_value} onChange={this.handleValueChange} /> €</td>
+                    <td> <button onClick={this.handleSave} id={this.state.asset._id}>save</button></td>
+                </tr>
+            )
+        else
+            return (
+                <tr>
+                    <td>{this.state.asset.asset_name}</td>
+                    <td>{this.state.asset.asset_value} €</td>
+                    <td>
+                        <button onClick={this.handleEdit}>edit</button>
+                        <button onClick={this.state.delete_function} id={this.state.asset._id}>sell or dispose</button>
+                    </td>
+                </tr>
+            )
+    }
+
+    handleNameChange(event:any) {
+        const newAsset = {
+            _id: this.state.asset._id,
+            asset_name: event.target.value,
+            asset_value: this.state.asset.asset_value
+        }
+        const newState:IState = {
+            delete_function: this.state.delete_function,
+            edit_mode: this.state.edit_mode,
+            asset:newAsset
+        }
+        this.setState(newState);
+    }
+
+    handleValueChange(event:any) {
+        this.setState({
+            asset: {
+                _id: this.state.asset._id,
+                asset_name: this.state.asset.asset_name,
+                asset_value: event.target.value
+            }
+        });
+    }
+
+    handleSave(event:any) {
+        this.setState({ edit_mode: false });
+    }
+    handleEdit() {
+        this.setState({ edit_mode: true });
+    }
+}
